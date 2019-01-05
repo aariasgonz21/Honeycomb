c8413a428cf487f6232a2c5f7a68b12883307258
github username <github email address>
2019-01-05 17:36:18 -0500

    logout & css

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -13,7 +13,7 @@ class CustomersController < ApplicationController
 
   def create
     @customer = Customer.create(customer_params)
-    byebug
+    #byebug
     if @customer.valid?
       redirect_to customer_path(@customer)
     else
